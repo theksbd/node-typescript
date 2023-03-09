ae44593846bc69dfe39a155c3b7d399a248da667
Do Thien Hoang <theksbd@gmail.com>
2023-03-09 23:07:53 +0700

    create basic app

--- server.ts
+++ server.ts
@@ -1,6 +1,9 @@
 import express, { Express, Request, Response } from 'express';
+import appRoute from './routes/appRoute';
 
 const app: Express = express();
+
+app.use('/api', appRoute);
 
 app.get('/', (req: Request, res: Response) => {
   res.send({ message: 'Hello World!!!' });
